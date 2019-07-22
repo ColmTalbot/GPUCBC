f0a934e0c818f4eea10a0b5c703a5c8465ef7020
Colm Talbot <colm.talbot@monash.edu>
2019-07-22 16:12:17 +1000

    make installable

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gpucbc",
+    version="0.0.1",
+    author="Colm Talbot",
+    author_email="colm.talbot@monash.edu",
+    description="GPU enabled CBC parameter estimation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ColmTalbot/GPUCBC",
+    packages=setuptools.find_packages(),
+    install_requires=['bilby', 'numpy', 'lalsuite'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
