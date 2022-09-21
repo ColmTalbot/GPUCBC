8bc7e270f8da08c9817a477b01c57b4c814f3a04
Colm Talbot <talbotcolm@gmail.com>
2022-09-21 15:25:26 -0400

    Add option to turn on jax

--- gpucbc/__init__.py
+++ gpucbc/__init__.py
@@ -1,4 +1,4 @@
-from . import likelihood, waveforms
+from . import likelihood, waveforms, pn
 
 
 def disable_cupy():
@@ -17,3 +17,21 @@ def enable_cupy():
         print("Cannot import cupy")
     likelihood.xp = xp
     waveforms.xp = xp
+
+
+def activate_jax():
+    from jax.config import config
+    from jax import numpy
+    config.update("jax_enable_x64", True)  # noqa
+    likelihood.np = numpy
+    likelihood.xp = numpy
+    waveforms.np = numpy
+    waveforms.xp = numpy
+    pn.np = numpy
+
+
+def deactivate_jax():
+    import numpy
+    likelihood.np = numpy
+    waveforms.np = numpy
+    pn.np = numpy
