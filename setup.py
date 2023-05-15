1bd93150c5e59a0dc5b487b2c0c20ecb22351010
Colm Talbot <talbotcolm@gmail.com>
2023-05-15 12:53:44 -0400

    PACKAGE: switch to pyproject

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="gpucbc",
-    version="0.1.0",
-    author="Colm Talbot",
-    author_email="colm.talbot@monash.edu",
-    description="GPU enabled CBC parameter estimation",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ColmTalbot/GPUCBC",
-    packages=["gpucbc"],
-    package_dir={"gpucbc": "gpucbc"},
-    install_requires=["numpy>=1.16.5", "astropy", "bilby>=2.0"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+setuptools.setup()
